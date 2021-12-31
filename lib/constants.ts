e7c88181dbc5c57058f61a2e3a27d6ca9d888376
roker15 <59526869+roker15@users.noreply.github.com>
2021-12-31 15:02:50 +0530

    analytics google

--- lib/constants.ts
+++ lib/constants.ts
@@ -2,8 +2,8 @@ export const NEXT_PUBLIC_SUPABASE_URL = process.env.NEXT_PUBLIC_SUPABASE_URL
 export const NEXT_PUBLIC_SUPABASE_ANON_KEY = process.env.NEXT_PUBLIC_SUPABASE_ANON_KEY
 // export const BASE_URL = "http://localhost:3000"
 // export const BASE_URL = "https://om-shree-ganeshay-namah-git-development4-roker15.vercel.app"
-export const BASE_URL = "http://localhost:3000"
-// export const BASE_URL = "https://qlook.in"
+// export const BASE_URL = "http://localhost:3000"
+export const BASE_URL = "https://qlook.in"
 
 export const DEFAULT_AVATARS_BUCKET = 'avatars'
 
