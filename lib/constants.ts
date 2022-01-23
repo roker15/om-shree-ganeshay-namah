ed32f8087715cbef02426ff5519ad1841674c11e
roker15 <59526869+roker15@users.noreply.github.com>
2022-01-23 10:25:22 +0530

    lkjlkjs

--- lib/constants.ts
+++ lib/constants.ts
@@ -4,7 +4,7 @@ export const NEXT_PUBLIC_SUPABASE_ANON_KEY = process.env.NEXT_PUBLIC_SUPABASE_AN
 // export const BASE_URL = "https://om-shree-ganeshay-namah-git-development4-roker15.vercel.app"
 // export const BASE_URL = "http://localhost:3000"
 // export const BASE_URL = "https://qlook.in";
-export const BASE_URL = "https://jionotes.com";
+export const BASE_URL = "https://jionote.com";
 
 export const DEFAULT_AVATARS_BUCKET = "avatars";
 
