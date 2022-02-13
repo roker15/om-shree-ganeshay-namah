d17cf18167a5b380b8a17dc04f06cc2659c0686c
roker15 <59526869+roker15@users.noreply.github.com>
2022-02-13 09:48:50 +0530

    456

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -27,12 +27,7 @@ function MyApp({ Component, pageProps }: AppLayoutProps) {
   return (
     <>
       <Head>
-        <title>Qlook - Target Strategically</title>
-        <link rel="icon" type="image/png" sizes="32x32" href="/logo-150x150.png" />
-
-        {/* <link rel="icon" type="image/png" sizes="16x16" href="/favicon-16x16.png" />
-          <link rel="apple-touch-icon" sizes="180x180" href="/apple-touch-icon.png" />
-          <link rel="manifest" href="/site.webmanifest" />
+        <title>Jionote</title> <link rel="icon" type="image/png" sizes="32x32" href="/logo-150x150.png" /> {/* <link rel="icon" type="image/png" sizes="16x16" href="/favicon-16x16.png" /> <link rel="apple-touch-icon" sizes="180x180" href="/apple-touch-icon.png" /> <link rel="manifest" href="/site.webmanifest" />
           <link rel="mask-icon" href="/safari-pinned-tab.svg" color="#5bbad5" />
           <meta name="theme-color" content="#ffffff" /> */}
       </Head>
