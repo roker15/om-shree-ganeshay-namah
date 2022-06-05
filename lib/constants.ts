b15a00ce4ddbd7654ce1a731c1b9b814c7da52b2
roker15 <59526869+roker15@users.noreply.github.com>
2022-06-05 09:13:50 +0530

    o

--- lib/constants.ts
+++ lib/constants.ts
@@ -4,8 +4,8 @@ export const NEXT_PUBLIC_SUPABASE_ANON_KEY = process.env.NEXT_PUBLIC_SUPABASE_AN
 // export const BASE_URL = "https://om-shree-ganeshay-namah-git-development4-roker15.vercel.app"
 // export const BASE_URL = "http://localhost:3000";
 // export const BASE_URL = "https://qlook.in";
-export const BASE_URL = "https://jionote.com";
-// export const BASE_URL = "https://om-shree-ganeshay-namah-git-development4-start-roker15.vercel.app/";
+// export const BASE_URL = "https://jionote.com";
+export const BASE_URL = "https://om-shree-ganeshay-namah-git-development4-start-roker15.vercel.app/";
 
 // npx openapi-typescript https://hbvffqslxssdbkdxfqop.supabase.co/rest/v1/?apikey=eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJyb2xlIjoiYW5vbiIsImlhdCI6MTYzMDIyNjQxOCwiZXhwIjoxOTQ1ODAyNDE4fQ.CCZ3y_Mzp5HjQJnuEXqL5Wq4tk2ZjZj97gVkODFYNh4 --output types/supabase.ts
 
