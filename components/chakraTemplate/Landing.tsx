b054209e78773532c63aa17c48fbbea3b7b9fced
roker15 <59526869+roker15@users.noreply.github.com>
2022-09-10 11:26:51 +0530

    as

--- components/chakraTemplate/Landing.tsx
+++ components/chakraTemplate/Landing.tsx
@@ -68,6 +68,9 @@ export default function CallToActionWithVideo() {
           Online Notes making platform
         </Text>{" "}
         for UPSC, NCERT, Current Affairs etc.{" "}
+        <Link isExternal color="telegram.400" fontWeight="bold" href="https://youtu.be/XD3Rr1nLTkY">
+          Watch Demo Video
+        </Link>
       </Heading>
       <Stack flex={3} spacing={{ base: "2", md: 2 }} py={{ base: "16", md: 16 }} alignItems={"center"}>
         <button
@@ -90,12 +93,12 @@ export default function CallToActionWithVideo() {
         <br />
         <br />
         <Text color={"gray.600"}>7000+ UPSC aspirants, 25000+ students using Jionote</Text>
-        <Text fontWeight={"extrabold"} fontSize={{ base: "2xl", sm: "3xl", lg: "3xl" }}>
+        {/* <Text fontWeight={"extrabold"} fontSize={{ base: "2xl", sm: "3xl", lg: "3xl" }}>
           Not Sure How to Use?{" "}
           <Link isExternal color="teal.400" href="https://youtu.be/XD3Rr1nLTkY">
             Watch Demo Video
           </Link>
-        </Text>
+        </Text> */}
         {/* <Stack spacing={{ base: 4, sm: 6 }} direction={{ base: "column", sm: "row" }}> */}
 
         {/* </Stack> */}
