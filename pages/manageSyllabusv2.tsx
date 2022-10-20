815226ba949ce8b3f749f6ed2b4c8e672f19b747
roker15 <59526869+roker15@users.noreply.github.com>
2022-10-20 09:56:45 +0530

    as

--- pages/manageSyllabusv2.tsx
+++ pages/manageSyllabusv2.tsx
@@ -1,13 +1,14 @@
-import { Box, VStack, Text } from "@chakra-ui/react";
-import { useUser } from "@supabase/auth-helpers-react";
+import { Box, Flex, IconButton, Text, VStack } from "@chakra-ui/react";
+import { useSupabaseClient, useUser } from "@supabase/auth-helpers-react";
 import axios from "axios";
 import React, { useEffect, useState } from "react";
+import { MdAdd, MdLightMode } from "react-icons/md";
 import useSWR from "swr";
-import CreateBookSyllabus from "../components/syllabus/CreateBookSyllabus";
 import LayoutWithTopNavbar from "../layout/LayoutWithTopNavbar";
+import { Database } from "../lib/database";
 import { useAuthContext } from "../state/Authcontext";
 import PageWithLayoutType from "../types/pageWithLayout";
-import { Data } from "./api/prisma/posts/posts";
+import { Data } from "./api/prisma/syllabus/posts";
 
 const ManageSyllabusv2: React.FunctionComponent = () => {
   const { profile } = useAuthContext();
@@ -31,29 +32,45 @@ const ManageSyllabusv2: React.FunctionComponent = () => {
   return (
     <Box minW="full">
       {user && profile?.role === "ADMIN" && (
-        <VStack>
-          Welcome
-          <Box>{data?.book_name}</Box>
-          <Box>
-            {data?.books_headings.map((headings, index) => (
-              <Box key={headings.id}>
-                <Text
-                  as="b"
-                  casing={"capitalize"}
-                  onClick={() => setselectedHeading(selectedHeading !== headings.id ? headings.id : undefined)}
-                  cursor="pointer"
-                >
-                  {headings.heading}
-                </Text>
-                <VStack alignItems={"left"} px="4" display={selectedHeading === headings.id ? "block" : "none"} width="80">
-                  {headings.books_subheadings.map((subheading) => (
-                    <Text key={subheading.id} casing={"capitalize"}>
-                      {subheading.subheading}
+        <VStack display="inline-block" w="80">
+          <Box p="2" bg="brand.50">
+            {data?.book_name}
+          </Box>
+          <Box display="inline-block">
+            <VStack alignItems="left" spacing="4">
+              {data?.books_headings.map((headings, index) => (
+                <VStack key={headings.id} alignItems="left">
+                  <Flex
+                    alignItems={"baseline"}
+                    onClick={() => setselectedHeading(selectedHeading !== headings.id ? headings.id : undefined)}
+                  >
+                    <IconButton
+                      variant="ghost"
+                      icon={selectedHeading !== headings.id ? <MdAdd /> : <MdLightMode />}
+                      aria-label={""}
+                    ></IconButton>
+                    <Text as="b" casing={"capitalize"} cursor="pointer">
+                      {headings.heading}
                     </Text>
-                  ))}
+                  </Flex>
+                  <VStack
+                    alignItems={"left"}
+                    pl="8"
+                    display={selectedHeading === headings.id ? "block" : "none"}
+                    spacing="4"
+                  >
+                    {headings.books_subheadings.map((subheading) => (
+                      <Flex key={subheading.id}>
+                        <Text fontSize={"sm"} casing={"capitalize"}>
+                          {subheading.subheading}
+                        </Text>
+                        {profile && profile.id && <ArticleCounter subheadingId={subheading.id} creatorId={profile.id} />}
+                      </Flex>
+                    ))}
+                  </VStack>
                 </VStack>
-              </Box>
-            ))}
+              ))}
+            </VStack>
           </Box>
         </VStack>
       )}{" "}
@@ -63,3 +80,28 @@ const ManageSyllabusv2: React.FunctionComponent = () => {
 
 (ManageSyllabusv2 as PageWithLayoutType).layout = LayoutWithTopNavbar;
 export default ManageSyllabusv2;
+
+export const ArticleCounter = ({ subheadingId, creatorId }: { subheadingId: number; creatorId: string }) => {
+  const supabaseClient = useSupabaseClient<Database>();
+  const [count, setCount] = useState<number | undefined>(undefined);
+  const getArticleCount = async () => {
+    const { data, error, count } = await supabaseClient
+      .from("books_articles")
+      .select("*", { count: "exact", head: true })
+      .match({ books_subheadings_fk: subheadingId, created_by: creatorId });
+    if (count) {
+      setCount(count);
+    }
+  };
+  useEffect(() => {
+    getArticleCount();
+  }, []);
+
+  return (
+    <Flex alignItems={"center"} px="2">
+      <Text as={"label" && "b"} fontSize="12px">
+        {count}
+      </Text>
+    </Flex>
+  );
+};
