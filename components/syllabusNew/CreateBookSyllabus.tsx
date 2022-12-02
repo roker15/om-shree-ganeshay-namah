5f1c04e297f40bae9bab53c3871e051ccba521a7
roker15 <59526869+roker15@users.noreply.github.com>
2022-12-02 19:59:17 +0530

    as

--- components/syllabusNew/CreateBookSyllabus.tsx
+++ components/syllabusNew/CreateBookSyllabus.tsx
@@ -1,7 +1,6 @@
 import { Box, Flex, VStack, Text } from "@chakra-ui/react";
 import React, { useState } from "react";
 import { BookResponse } from "../../types/myTypes";
-import BookFilter from "./BookFilterForMangeSyllabus";
 import FormCreateHeading from "./FormCreateHeading";
 import FormCreateSubheading from "./FormCreateSubheading";
 import Syllabus from "./Syllabus";
