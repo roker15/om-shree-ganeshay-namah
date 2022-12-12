ca7d476b2a336e010b86d2f396647eb81fe18204
roker15 <59526869+roker15@users.noreply.github.com>
2022-12-12 07:16:58 +0530

    as

--- customHookes/apiHooks.tsx
+++ customHookes/apiHooks.tsx
@@ -16,8 +16,6 @@ const cacheOptions = {
 };
 
 export function useGetSyllabusByBookId(bookId: number | undefined) {
-  const supabaseClient = useSupabaseClient<Database>();
-
   const fetcher = async () => {
     const response = await axios.get<Data>("/api/prisma/syllabus/syllabus", { params: { bookId: bookId } }).catch((e) => {
       throw e;
@@ -38,7 +36,6 @@ export function useGetSyllabusByBookId(bookId: number | undefined) {
   };
 }
 export function useGetArticleCount(subheadingId: number, creatorId: string) {
-
   const fetcher = async () => {
     const response = await axios
       .get<number>("/api/prisma/posts/postcountbysyllabuss", { params: { subheadingId, creatorId } })
@@ -127,6 +124,7 @@ export function useGetArticlesbyUserandSubheading(props: { subheadingId: number;
   };
 }
 export function useGetLatestCurrentaffairs() {
+  const supabaseClient = useSupabaseClient<Database>();
   const fetcher = async () => {
     const response = await axios
       .get<ApiLatestCurrentAffairs[]>("/api/prisma/posts/getlatestcurrentaffairs", {
@@ -136,6 +134,16 @@ export function useGetLatestCurrentaffairs() {
         throw e;
       });
     return response.data;
+  };
+  const fetcher1 = async () => {
+    const { data, error } = await supabaseClient.from("books_articles").select(`id: number;
+sequence,
+article_title,
+current_affair_tags,
+question_type,
+question_year,
+created_at,
+created_by`);
   };
   const { data, error, mutate, isValidating } = useSWR(["/api/prisma/posts/getlatestcurrentaffairs"], fetcher, cacheOptions);
   return {
